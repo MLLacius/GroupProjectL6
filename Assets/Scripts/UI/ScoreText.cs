3c1bc8e88d03672044f99fddb9c72168575ee1b9
MLLacius <collinsleyton@gmail.com>
2026-02-02 10:54:07 +0000

    Achieving a new high score will now give visual feedback during gameplay
    High score is no longer displayed during gameplay, the text object has been repurposed into a pop-up that flashes when a new high score is achieved along with the score text changing colour.
    
    Will look into adding audio and potentially particle feedback next, alongside putting feedback for beating high score onto the game over screen.

--- Assets/Scripts/UI/ScoreText.cs
+++ Assets/Scripts/UI/ScoreText.cs
@@ -8,37 +8,53 @@ public class ScoreText : MonoBehaviour
     [SerializeField] private TMP_Text highScoreText;
     [SerializeField] private GameMaster gameMaster;
 
+    private void Start()
+    {
+        highScoreText.canvasRenderer.SetAlpha(0f);
+    }
+
     // Update is called once per frame
     void Update()
     {
         if(gameMaster && scoreText &&  highScoreText)
         {
             scoreText.text = "Score: " + gameMaster.GetCurrentScore().ToString();
+            /*
             if (gameMaster.HasAchievedHighScore())
             {
                 highScoreText.text = "High Score: " + gameMaster.GetHighScore().ToString();
             }
+            */
         }
     }
 
     public void EnableUI()
     {
+        /*
         int highScore = gameMaster.GetHighScore();
         highScoreText.text = "High Score: " + highScore.ToString();
         Debug.Log(highScore);
+        */
     }
 
     public void HighScoreAchievedGameplay()
     {
-        Color startingColor = highScoreText.color;
         StartCoroutine(FlashHighScore());
-        highScoreText.color = startingColor;
     }
 
     private IEnumerator FlashHighScore()
     {
+        Color startingColor = scoreText.color;
+        scoreText.color = new Color(0.918f, 0.718f, 0f);
+        for (int i = 0; i < 6; i++)
+        {
+            highScoreText.canvasRenderer.SetAlpha(1f);
+            yield return new WaitForSeconds(0.3f);
+            highScoreText.canvasRenderer.SetAlpha(0f);
+            yield return new WaitForSeconds(0.3f);
+        }
 
-
+        scoreText.color = startingColor;
         yield return null;
     }
 }
